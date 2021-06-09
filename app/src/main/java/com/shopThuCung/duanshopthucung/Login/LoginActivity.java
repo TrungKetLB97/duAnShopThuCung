61d695e1a707073db9203ab3e601c0fdab784b20
nctrung2001 <Nguyenchitrung2001@gmail.com>
2021-06-09 14:34:26 +0700

    Login

--- app/src/main/java/com/shopThuCung/duanshopthucung/Login/LoginActivity.java
+++ app/src/main/java/com/shopThuCung/duanshopthucung/Login/LoginActivity.java
@@ -33,25 +33,19 @@ public class LoginActivity extends AppCompatActivity {
         passwordDAO_nv = new PasswordDAO_NV(LoginActivity.this);
 
 
-        btnLoginNV.setOnClickListener(v ->
-                validateLoginNV(edtId.getText().toString(),edtPass.getText().toString()));
+        //btnLoginNV.setOnClickListener(v ->
+          //      validateLoginNV(edtId.getText().toString(),edtPass.getText().toString()));
     }
 
     public void validateLoginAdmin(String idAd, String passAd){
-        boolean var = passwordDAO_admin.checkUser(idAd,passAd);
-        if(var){
+        boolean varAd = passwordDAO_admin.checkUser(idAd,passAd);
+        boolean varNv = passwordDAO_nv.checkUser(idAd,passAd);
+        if(varAd){
             Intent i = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(i);
             finish();
         }
-        else{
-            Toast.makeText(this, "id or password is incorrect, try again", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void validateLoginNV(String idAd, String passAd){
-        boolean var = passwordDAO_nv.checkUser(idAd,passAd);
-        if(var){
+        else if(varNv){
             Intent i = new Intent(LoginActivity.this, MainActivity_NV.class);
             startActivity(i);
         }
@@ -59,6 +53,12 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this, "id or password is incorrect, try again", Toast.LENGTH_SHORT).show();
         }
     }
+
+//    public void validateLoginNV(String idAd, String passAd){
+//        else{
+//            Toast.makeText(this, "id or password is incorrect, try again", Toast.LENGTH_SHORT).show();
+//        }
+//    }
 
     public void LoginAdmin(View view) {
         validateLoginAdmin(edtId.getText().toString(),edtPass.getText().toString());
